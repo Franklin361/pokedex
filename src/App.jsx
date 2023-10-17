81613c5fcf33683aa235df23e957e036566d5f79
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2023-10-17 09:44:31 -0600

    feat: create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
+import { Header } from './components/header'
+
 const App = () => {
   return (
     <div className='container mx-auto py-5'>
-      <header>
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Pokédex ()
-        </h1>
-        <button className=''>collection</button>
-      </header>
+
+      <Header />
+
     </div>
   )
 }
 export default App
+
---
+++ src/components/header.jsx
@@ -0,0 +1,17 @@
+
+import { BsCollection } from 'react-icons/bs'
+
+export const Header = () => {
+  return (
+    <header className='flex justify-between items-center'>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Pokédex (VINCO ORBIS)
+      </h1>
+      <button className='p-3 flex justify-center items-center gap-2 bg-orange-400 hover:bg-orange-500 font-semibold text-white rounded-lg shadow-lg shadow-black/40'>
+        <BsCollection />
+        collection
+      </button>
+    </header>
+
+  )
+}
