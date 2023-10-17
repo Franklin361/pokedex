57776f241bd0d72f26f7cbc982f7913a4f194f8f
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2023-10-17 16:37:45 -0600

    design drawer

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,6 @@
 
-import { BsCollection } from 'react-icons/bs'
+
+import { Drawer } from './drawer'
 
 export const Header = () => {
   return (
@@ -7,10 +8,7 @@ export const Header = () => {
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Pokédex (VINCO ORBIS)
       </h1>
-      <button className='p-3 border border-black flex justify-center items-center gap-2 bg-orange-400 hover:bg-orange-500 font-semibold text-white rounded-lg shadow-lg shadow-black/40'>
-        <BsCollection />
-        collection
-      </button>
+      <Drawer />
     </header>
 
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,35 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "slide-in-fwd-left": "slide-in-fwd-left 0.4s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both",
+        "slide-out-left": "slide-out-left 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530)   both"
+      },
+      keyframes: {
+        "slide-out-left": {
+          "0%": {
+            transform: "translateX(0)",
+            opacity: "1"
+          },
+          to: {
+            transform: "translateX(-1000px)",
+            opacity: "0",
+            display: "none"
+          }
+        },
+        "slide-in-fwd-left": {
+          "0%": {
+            transform: "translateZ(-1400px) translateX(-1000px)",
+            opacity: "0"
+          },
+          to: {
+            transform: "translateZ(0) translateX(0)",
+            opacity: "1"
+          }
+        }
+      }
+    },
   },
   plugins: [],
 }
